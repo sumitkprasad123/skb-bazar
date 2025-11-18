821a3b65cd80c09919839c5beec6a4337f94748d
sumitkprasad123 <sumitkprasad123@gmail.com>
2025-11-18 23:07:05 +0530

    initial folder structure

---
+++ src/main/java/com/skb/bazar/SkbBazarApplication.java
@@ -0,0 +1,13 @@
+package com.skb.bazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkbBazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkbBazarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skb/bazar/SkbBazarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skb.bazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkbBazarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
